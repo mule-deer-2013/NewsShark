2497ad968197551d388309bb9089a473c80ab77e
Apprentice <apprentice@dbc19.local>
2013-09-26 23:56:42 -0700

    WIP: Create scraper method in Channel model. Works in rails console but not in the browser. Pending test.

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -3,5 +3,12 @@ require 'spec_helper'
 describe Channel do 
   it { should validate_presence_of :name }
   it { should belong_to :user }
+  context "#scrape_for_articles" do
+    it "returns titles and links of articles from Google news" do
+      pending
+    end
+  end
 end
+
+
 
