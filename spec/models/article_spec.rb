173f966ab194dc735460c4f8cda0f26c88984db7
Dev Bootcamp <Apprentice@dbc35.local>
2013-09-30 12:51:00 -0700

    channel learns about preferenced keywords based on whether article is liked or not.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,14 +4,26 @@ describe Article do
   it { should validate_presence_of :title }
   it { should validate_presence_of :url }
 
-  context "#set_keywords" do 
+  context "#set_keywords" do
     let(:article) { FactoryGirl.create :article }
 
-    it "sets keywords" do 
+    it "sets keywords" do
       # This test is actually hitting up a real URL... Probably needs VCR (or a 'fixture')
       expect{
         article.set_keywords
-      }.to change { article.keywords } 
+      }.to change { article.keywords }
     end
   end
+
+  context "#update_user_feedback" do
+    let(:article) { FactoryGirl.create :article }
+    it "updates user feedback with argument" do
+      expect {
+        article.update_user_feedback(1)
+      }.to change {
+        article.user_feedback
+      }.from(nil).to(1)
+    end
+  end
+
 end
