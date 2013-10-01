4fa141719e81b8c22304cacf98e0c07fb9e2747e
Apprentice <apprentice@dbc19.local>
2013-09-30 22:52:10 -0700

    refactor recommender

--- app/models/recommender.rb
+++ app/models/recommender.rb
@@ -3,11 +3,13 @@ class Recommender < ActiveRecord::Base
   def self.rank_closeness(channel_id)
     channel = Channel.find(channel_id)
     closeness = {}
-    if channel.numfeedback >= 3
-      channel.articles.each do |article|
+    scaling_factor = 3
+    minimum_feedback = 3
+    if channel.rated_articles_count >= minimum_feedback
+      channel.articles.where( user_feedback: nil ).each do |article|
         article_closeness = 0
         channel.preferred_keywords.each_pair do |term, amplitude|
-          article_closeness += (amplitude/(channel.numfeedback/3.to_f)).ceil if article.keywords.include?(term)
+          article_closeness += (amplitude.to_i/(channel.rated_articles_count/scaling_factor.to_f)).ceil if article.keywords.include?(term)
         end 
         closeness[article.id] = article_closeness
       end
