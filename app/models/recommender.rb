f5c1f443dcf821dbc841adf21cc9ce96b023fb1b
Dev Bootcamp <Apprentice@dbc35.local>
2013-09-30 20:08:14 -0700

    comment about AR base on Recommender model.

--- app/models/recommender.rb
+++ app/models/recommender.rb
@@ -1,4 +1,4 @@
-class Recommender < ActiveRecord::Base
+class Recommender < ActiveRecord::Base # Does this need to be AR::Base?
 
   def self.rank_closeness(channel_id)
     channel = Channel.find(channel_id)
@@ -8,7 +8,7 @@ class Recommender < ActiveRecord::Base
         article_closeness = 0
         channel.preferred_keywords.each_pair do |term, amplitude|
           article_closeness += (amplitude/(channel.numfeedback/3.to_f)).ceil if article.keywords.include?(term)
-        end 
+        end
         closeness[article.id] = article_closeness
       end
     end
