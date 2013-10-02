0383f0cf5dc9e2f56d74b0d4030fc7722c876f6c
Dev Bootcamp <Apprentice@dbc35.local>
2013-10-02 13:47:11 -0700

    sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 NewsShark::Application.routes.draw do
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:create]
+  delete '/signout', to: 'sessions#destroy', as: :signout
 
-  match '/signin', to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy'
-
-  resources :users, only: [:new, :create, :show] do
+  # JW: only nest resources when it doesn't make sense to work with the sub-resource
+  #     without also having a hold of the parent resource -- is this true for channels
+  #     and articles?
+  resources :users, only: [:new, :create] do
     resources :channels, only: [:create, :show, :destroy] do
         resources :articles, only: [:update]
     end
   end
+  get '/profile', to: 'users#show', as: :user
 
   root to: 'users#show'
 
