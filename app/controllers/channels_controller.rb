83193bf1ff0749ab0e2df03e48c3e415a11e34fc
Apprentice <apprentice@dbc19.local>
2013-10-01 23:14:17 -0700

    move channel scrape in controller to create, instead of happening in show it will now be done as a rake task

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,6 +3,7 @@ class ChannelsController < ApplicationController
     channel = Channel.new(params[:channel])
     channel.user = User.find(params[:user_id])
     if channel.save
+      channel.scrape_for_articles
       redirect_to user_channel_path(channel.user, channel)
     else
       flash[:notice] = channel.errors.full_messages.join(', ')
@@ -12,7 +13,6 @@ class ChannelsController < ApplicationController
 
   def show
     @channel = Channel.find(params[:id])
-    @channel.scrape_for_articles
     best_articles = Recommender.best_articles_ranked(@channel.id)
     @articles = ( best_articles.empty? ? @channel.articles : best_articles )
   end
