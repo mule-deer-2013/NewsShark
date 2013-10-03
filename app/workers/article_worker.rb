99eee66bd7b642516c2212c945709ca055e1e1bb
Apprentice <apprentice@dbc19.local>
2013-10-03 10:42:37 -0700

    set word_count ranges

--- app/workers/article_worker.rb
+++ app/workers/article_worker.rb
@@ -5,25 +5,17 @@ class ArticleWorker
   def perform(article_id)
     article = Article.find(article_id)
     attributes = ArticleScraper.get_article_attributes(article.url)
-    # puts "%" * 50  
-    # puts "These are the attributes"
-    # puts attributes
-
-    # puts "&" * 50 
-    # puts "This is kincaid"
-    # puts attributes[:kincaid]
-    # puts "*" * 50
-    # puts "worker"
-    # puts attributes
-    # puts "*" * 50
-    # puts attributes[:kincaid]
-    article.kincaid = attributes[:kincaid]
-    article.word_count = attributes[:word_count]
-    article.author = attributes[:author]
-    article.keywords = attributes[:keywords]
-    article.datetime = attributes[:datetime]
-    article.description = attributes[:description]
-    article.save
+    # article.kincaid = attributes[:kincaid]
+    # article.word_count = attributes[:word_count]
+    # article.author = attributes[:author]
+    # article.keywords = attributes[:keywords]
+    # article.datetime = attributes[:datetime]
+    # article.description = attributes[:description]
+    # article.save
+    # article.update_attributes( attributes )
+    attributes.each do |attribute, value|
+      article.update_attribute(attribute, value)
+    end
   end
 
 end
