1a2ca52d0b354596d78a4ac23ff250da52389ff5
Apprentice <apprentice@dbc19.local>
2013-10-03 11:35:08 -0700

    test article_scraper.rb

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'mechanize'
-gem 'pismo'
 gem 'lingua'
+gem 'pismo'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/models/article_scraper.rb
+++ app/models/article_scraper.rb
@@ -5,7 +5,7 @@ class ArticleScraper
     reading_level_ranges = { (0..8.82) => 1, (8.82..10.3) => 2, (10.3..11.73) => 3, (11.73..13.15) => 4, (13.15..100) => 5}
     word_count_ranges = { (1..185) => 1, (185..280) => 2, (280..405) => 3, (405..675) => 4, (675..5000) => 5}
 
-    doc = Pismo[url]
+    doc = Pismo::Document.new(url)
     article_attributes[:author] = doc.author
     article_attributes[:keywords] = Hash[doc.keywords].keys
     stats = Lingua::EN::Readability.new(doc.body)
@@ -13,7 +13,7 @@ class ArticleScraper
     article_attributes[:description] = doc.description
     word_count = doc.body.split(' ').count
     word_count_ranges.each_pair do |range, quintile|
-        article_attributes[:word_count] = quintile if range.include?(word_count)
+        article_attributes[:word_count] = quintile if word_count.in?(range)
     end
     reading_level_ranges.each_pair do |range, quintile|
       article_attributes[:kincaid] = quintile if range.include?(stats.kincaid)
