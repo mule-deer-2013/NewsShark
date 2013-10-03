00590c03978a8853da886a872a9bf3cc9ce16f88
Apprentice <apprentice@dbc19.local>
2013-10-03 12:15:49 -0700

    add parallel fields from articles to channels. adjust channel_article_mapper constants.

--- app/models/channel_article_mapper.rb
+++ app/models/channel_article_mapper.rb
@@ -1,14 +1,22 @@
 module ChannelArticleMapper
 
   ATTRIBUTE_MAPPER = {  :preferenced_keywords => :keywords ,
-                        :preferenced_publications => :publication }
+                        :preferenced_publications => :publication,
+                        :preferenced_authors => :author,
+                        :preferenced_word_counts => :word_count,
+                        :preferenced_kincaids => :kincaid
+                      }
 
   PREFERENCED_ATTRIBUTES = ATTRIBUTE_MAPPER.keys
 
   KARMA_SCALING_FACTOR = 10.0
 
-  KARMA_WEIGHTS = { 'publication' => 0.70,
-                    'keyword' => 0.30 }
+  KARMA_WEIGHTS = { 'publication' => 0.25,
+                    'keyword' => 0.15,
+                    'author' => 0.30,
+                    'kincaid' => 0.20,
+                    'word_count' => 0.10
+                  }
 
   MINIMUM_FEEDBACK = 3
 
