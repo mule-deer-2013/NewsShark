3a6a255a3f7b10d5433472c54adc359a83607b0d
Thomas Landon <thomasrlandon@gmail.com>
2013-10-25 14:01:32 -0700

    fix bug to make background jobs fire and succeed with Iron_worker instead of Sidekiq and Redis

--- lib/workers/article_worker.rb
+++ lib/workers/article_worker.rb
@@ -1,11 +1,16 @@
 require 'lingua'
 require 'pismo'
-require 'article_scraper.rb'
+require '../../app/models/article_scraper.rb'
+require 'postgres_ext'
 require 'pg'
 require 'active_record'
-require 'channel_article_mapper.rb'
-require 'article.rb'
+require 'activerecord-postgres-hstore'
+require '../../app/models/channel_article_mapper.rb'
+require '../../app/models/article.rb'
 
+def params
+  {"article_id"=>356, "database"=>{"adapter"=>"postgresql", "database"=>"d6pfhpqd4ndt8n", "username"=>"jxfxkuolpjxwux", "password"=>"AZxlUDFdpRSG1aaQgbaJ1r-Yb3", "host"=>"ec2-23-21-113-206.compute-1.amazonaws.com", "port"=>5432}}
+end
 p params
 
 def setup_database
@@ -17,7 +22,6 @@ end
 
 
 setup_database
-
 
 def perform(article_id)
   article = Article.find(article_id)
